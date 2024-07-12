aaaab69f62879818a73fc12ff0598a651f890ba3
Camila Nepomuceno <camila.cno@gmail.com>
2024-07-12 14:38:38 -0300

    Update

--- main.js
+++ main.js
@@ -1,48 +1,63 @@
-const parallax = document.getElementById("home-img-lg");
-const parallax1 = document.getElementById("parallax1");
-const parallax2 = document.getElementById("parallax2");
+const parallax = document.getElementById('home-img-lg')
+const parallax1 = document.getElementById('parallax1')
+const parallax2 = document.getElementById('parallax2')
 
-window.addEventListener("scroll", function()
-{
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionX = offset*(-0.3)-100 + "px";
+function playAudio() {
+  var audio = document.getElementById('meuAudio')
+  if (audio) {
+    console.log('Elemento de áudio encontrado')
+    audio
+      .play()
+      .then(() => {
+        console.log('Áudio está tocando')
+      })
+      .catch((error) => {
+        console.log('O áudio não pôde ser reproduzido automaticamente: ', error)
+      })
+  } else {
+    console.log('Elemento de áudio não encontrado')
+  }
+}
+
+window.addEventListener('load', function () {
+  playAudio()
 })
 
-
-window.addEventListener("scroll", function()
-{
-    let offset = window.pageYOffset;
-    offset-=3100;
-    parallax1.style.backgroundPositionY = offset*(0.1) + "px";
+window.addEventListener('scroll', function () {
+  let offset = window.pageYOffset
+  // parallax.style.backgroundPositionX = offset * -0.3 - 100 + 'px'
 })
 
-window.addEventListener("scroll", function()
-{
-    let offset = window.pageYOffset;
-    offset-=4800;
-    parallax2.style.backgroundPositionY = offset*(-0.1) + "px";
+window.addEventListener('scroll', function () {
+  let offset = window.pageYOffset
+  offset -= 3100
+  // parallax1.style.backgroundPositionY = offset * 0.1 + 'px'
+})
+
+window.addEventListener('scroll', function () {
+  let offset = window.pageYOffset
+  offset -= 4800
+  // parallax2.style.backgroundPositionY = offset * -0.1 + 'px'
 })
 
 function myFunction() {
-    document.getElementById("check").checked = false;
-  }
-
-
-  
-function reveal() {
-var reveals = document.querySelectorAll(".reveal");
-  
-for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
+  document.getElementById('check').checked = false
 }
-  
-window.addEventListener("scroll", reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal')
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight
+    var elementTop = reveals[i].getBoundingClientRect().top
+    var elementVisible = 150
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add('active')
+    } else {
+      reveals[i].classList.remove('active')
+    }
+  }
+}
+
+window.addEventListener('scroll', reveal)
